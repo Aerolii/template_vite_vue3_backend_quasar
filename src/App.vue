63dabfae14b8eaaa90c2ee946e1276837ae6397f
zhangxiaobo <zhangxiaobo@yunji.ai>
2022-06-15 18:37:27 +0800

    fix: fix

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from 'vue'
+const a = ref(1)
 </script>
 
 <template>
